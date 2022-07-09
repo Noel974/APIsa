e08d86bfa9f84038442c620b235acbaadbad89c1
noel <noelantoine974@outlook.fr>
2022-07-09 21:10:32 +0400

    mongo

--- app.js
+++ app.js
@@ -2,22 +2,35 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=> {
-    console.log('Requete reçue!');
+const mongoose = require('mongoose');
+const userRoutes = require('./models/User');
+
+
+require('dotenv').config();
+
+// connexion a la base de données mongoDB
+mongoose.connect(`mongodb+srv://noel:Emmanuel@cluster0.pxrqail.mongodb.net/?retryWrites=true&w=majority`,
+    {useNewUrlParser: true,
+    useUnifiedTopology: true})
+    .then(()=> console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
+
+// Permet d'analyser le corps de la requête.
+app.use(express.json()); 
+
+app.use(helmet({ crossOriginResourcePolicy: { policy: "same-site" } }));
+
+// configuration des CORS doit être placé avant les routes de l'API
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH', 'OPTIONS');
     next();
-});
-
-app.use((req, res, next)=>{
-    res.status(201);
-    next();
-});
-
-app.use((req, res)=> {
-    res.json ({message: 'votre rquete a bien recue !'})
-});
-
-app.use((req, res)=> {
-    console.log ('Response envoyée avec sucès')
 })
+
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+// configuration des routes
+app.use('/api/auth', userRoutes);
 
 module.exports = app; 
