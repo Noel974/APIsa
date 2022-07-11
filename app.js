77d353a28e0d172bb0db9b1cc469ec33529c3723
noel <noelantoine974@outlook.fr>
2022-07-11 20:32:01 +0400

    correction

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require('express');
 
 const app = express();
-
+const helmet = require('helmet');
 const mongoose = require('mongoose');
-const userRoutes = require('./models/User');
+const userRoutes = require('./routes/User');
 
+const path = require('path');
 
 require('dotenv').config();
 
 // connexion a la base de données mongoDB
 mongoose.connect(`mongodb+srv://noel:Emmanuel@cluster0.pxrqail.mongodb.net/?retryWrites=true&w=majority`,
-    {useNewUrlParser: true,
-    useUnifiedTopology: true})
-    .then(()=> console.log('Connexion à MongoDB réussie !'))
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'))
 
 // Permet d'analyser le corps de la requête.
-app.use(express.json()); 
+app.use(express.json());
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: "same-site" } }));
 
@@ -24,7 +27,7 @@ app.use(helmet({ crossOriginResourcePolicy: { policy: "same-site" } }));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH', 'OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH', 'OPTIONS');
     next();
 })
 
